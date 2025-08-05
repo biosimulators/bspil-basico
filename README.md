e9ebd805aeeb3369b9cdda5c9e2d7bc77ccb86ac
Logan Drescher <drescher@uchc.edu>
2025-08-05 14:57:42 -0400

    basic structure and legacy test

---
+++ README.md
@@ -0,0 +1,3 @@
+# BioSimulators Process Interface Library: Basico (COPASI)
+
+`BSpil: Basico` is a BioSimulators project designed to wrap Tellurium in process wrappers at various layers
---
+++ bspil_basico/foundation/foundationCompartmentArrayTools.py
@@ -0,0 +1,25 @@
+import numpy as np
+import basico as bsc
+from process_bigraph import Process, Composite, pf, Step
+
+class compartmentArrayTools_animateRectangularTimeCourseStep(Step):
+    pass
+
+class compartmentArrayTools_animateRectangularTimeCourseAsImageStep(Step):
+    pass
+
+class compartmentArrayTools_createLinearArrayStep(Step):
+    pass
+
+class compartmentArrayTools_createRectangularArrayStep(Step):
+    pass
+
+class compartmentArrayTools_DeleteCompartmentsStep(Step):
+    pass
+
+class compartmentArrayTools_PlotLinearTimeCourseStep(Step):
+    pass
+
+class compartmentArrayTools_PlotRectangularTimeCourseStep(Step):
+    pass
+
