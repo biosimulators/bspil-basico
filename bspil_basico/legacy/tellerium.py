e11ce3a53e0c761f3fc9eef8917007927577ee4c
Ezequiel Valencia <ezq.valencia@gmail.com>
2025-10-16 12:34:18 -0400

    Update Tellurium Step

--- bspil_basico/legacy/tellerium.py
+++ bspil_basico/legacy/tellerium.py
@@ -8,35 +8,36 @@ from roadrunner.roadrunner import RoadRunner
 
 
 class TelluriumTimeCourseStep(Step):
-    runner: RoadRunner
+    runner: RoadRunner = None
     output_dir: str
+    sedml: str = None
 
     def __init__(self, config, core):
         super().__init__(config, core)
 
     def initialize(self, config):
-        sbml_file_path: str = os.path.abspath(os.path.expanduser(config['sbml_file_path']))
-        if not os.path.exists(sbml_file_path):
-            raise FileNotFoundError(sbml_file_path)
+        if config['sbml_file_path'] is not None:
+            sbml_file_path: str = os.path.abspath(os.path.expanduser(config['sbml_file_path']))
+            if not os.path.exists(sbml_file_path):
+                raise FileNotFoundError(sbml_file_path)
 
-        if config['output_dir'] is None:
-            raise ValueError('`output_dir` cannot be None')
-        output_dir = os.path.abspath(os.path.expanduser(config['output_dir']))
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-        self.output_dir = output_dir
+            if config['output_dir'] is None:
+                raise ValueError('`output_dir` cannot be None')
+            output_dir = os.path.abspath(os.path.expanduser(config['output_dir']))
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            self.output_dir = output_dir
 
-        self.runner = te.loadSBMLModel(sbml_file_path)
-        return
+            self.runner = te.loadSBMLModel(sbml_file_path)
+        else:
+            raise ValueError('`sbml_file_path` cannot be None')
 
     def update(self, state):
         num_data_points: int = state["num_data_points"]
         starting_time: float = state["starting_time"]
         end_time: float = state["end_time"]
-        results: pd.DataFrame = self.runner.simulate(start=starting_time, end=end_time, points=num_data_points)
-        results.plot()
-        plt.savefig(os.path.join(self.output_dir, "plot.pdf"))
-        results.to_csv(os.path.join(self.output_dir, "report.csv"))
+        output_file = self.output_dir + "/report.csv"
+        self.runner.simulate(start=starting_time, end=end_time, points=num_data_points, output_file=output_file)
         return {}
 
     def inputs(self):
